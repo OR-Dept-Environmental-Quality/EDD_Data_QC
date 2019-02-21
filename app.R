d2cd2bf11fbfae2b2358a67773eb27bde5df4d9c
Aliana Britson <Britson.Aliana@deq.state.or.us>
2019-02-20 16:26:28 -0800

    cleaning out characteristics, don't need to query on these

--- app.R
+++ app.R
@@ -26,68 +26,7 @@ source("Validation_Function.R")
 
 
 # Query out the valid values ---------------------------------------------
-
-#NPDES only needs a limited # of Chars, this should help speed up the program
-
-#make it so only the groupings are shown in the drop down, 
-#have the actual characteristics in separate variables to be called by program
-chars <- c("All RPA","All Toxics","Copper BLM","ph RPA","Ammonia RPA","DO RPA","Pesticides and PCB RPA","Base Neutral RPA", "Acid Extractable RPA",
-           "VOC RPA","Metals RPA")
-
-#RPA specific characteristics
-#pH RPA
-phrpa<-c("Alkalinity, total","pH","Temperature, water","Salinity","Conductivity")
-
-#Ammonia RPA
-ammrpa<-c("Alkalinity, total","Ammonia ","Ammonia and ammonium","Ammonia-nitrogen","Conductivity","pH","Temperature, water","Salinity")
-
-#Copper BLM
-cuB<-c("Alkalinity, total","Calcium","Chloride","Copper","Magnesium","pH","Potassium","Sodium","Sulfate","Organic carbon",
-       "Temperature, water","Total Sulfate","Sulfide")
-
-#Dissolved Oxygen RPA
-dorpa<-c("Dissolved oxygen (DO)","Dissolved oxygen saturation","Biochemical oxygen demand, non-standard conditions",
-         "Biochemical oxygen demand, standard conditions","Kjeldahl nitrogen","Total Kjeldahl nitrogen","Temperature, water",
-         "Ammonia ","Ammonia and ammonium","Ammonia-nitrogen")  
-
-#Pesticides and PCBs
-pestrpa<-c("p,p'-DDT","Parathion","Chlordane","Lindane","Dieldrin","Endrin","Methoxychlor","p,p'-DDD","p,p'-DDE","Heptachlor",
-           "Azinphos-methyl","Malathion","Aldrin",".alpha.-Hexachlorocyclohexane",".beta.-Hexachlorocyclohexane",
-           "Benzene Hexachloride, Beta (BHC)","1,2,3,4,5,6-Hexachlorocyclohexane",".alpha.-Endosulfan","Heptachlor epoxide",
-           "Endosulfan sulfate","Mirex","Chlorpyrifos","Endrin aldehyde","Toxaphene","Demeton","Aroclor 1260","Aroclor 1254",
-           "Aroclor 1221","Aroclor 1232","Aroclor 1248","Aroclor 1016",".beta.-Endosulfan","Aroclor 1242")
-
-#Base Neutral
-bneut<-c("Benzo[a]pyrene","Dibenz[a,h]anthracene","Benz[a]anthracene","N-Nitrosodimethylamine","Hexachloroethane",
-         "Hexachlorocyclopentadiene","Isophorone","Acenaphthene","Diethyl phthalate","Dibutyl phthalate","Phenanthrene",
-         "Butyl benzyl phthalate","N-Nitrosodiphenylamine","Fluorene","Hexachlorobutadiene","Naphthalene","2-Chloronaphthalene",
-         "3,3'-Dichlorobenzidine","Benzidine","1,2,4,5-Tetrachlorobenzene","Nitrobenzene","p-Bromophenyl phenyl ether",
-         "Bis(2-chloro-1-methylethyl) ether","Bis(2-chloroethyl) ether","Bis(2-chloroethoxy)methane","Di(2-ethylhexyl) phthalate",
-         "Di-n-octyl phthalate","Hexachlorobenzene","Anthracene","1,2,4-Trichlorobenzene","2,4-Dinitrotoluene","1,2-Diphenylhydrazine",
-         "Pyrene","Dimethyl phthalate","Benzo[ghi]perylene","Indeno[1,2,3-cd]pyrene","Benzo(b)fluoranthene","Fluoranthene",
-         "Benzo[k]fluoranthene","Acenaphthylene","Chrysene","2,6-Dinitrotoluene","Pentachlorobenzene","N-Nitrosodi-n-propylamine",
-         "p-Chlorophenyl phenyl ether")
-
-#Acid Extractable
-aext<-c("2,4-Dinitrophenol","p-Chloro-m-cresol","Pentachlorophenol","2,4,6-Trichlorophenol","o-Nitrophenol","o-Chlorophenol",
-        "2,4,5-Trichlorophenol","p-Nitrophenol","2,4-Dimethylphenol","Phenol","2,4-Dichlorophenol","4,6-Dinitro-o-cresol")
-
-#Volatile Organic Carbons
-vocrpa<-c("Carbon tetrachloride","Chloroform","Benzene","1,1,1-Trichloroethane","Methyl bromide","Chloromethane","Chloroethane",
-          "Vinyl chloride","Methylenechloride","Tribromomethane","Dichlorobromomethane","1,1-Dichloroethane","1,1-Dichloroethylene",
-          "1,2-Dichloropropane","1,1,2-Trichloroethane","Trichloroethene(TCE)","1,1,2,2-Tetrachloroethane","o-Dichlorobenzene",
-          "Ethylbenzene","p-Dichlorobenzene","Acrolein","Allyl chloride","1,2-Dichloroethane","Toluene","Chlorobenzene",
-          "2-Chloroethyl vinyl ether","Chlorodibromomethane","Tetrachloroethene","Tetrachloroethylene","trans-1,2-Dichloroethylene",
-          "m-Dichlorobenzene","1,3-Dichloropropene")
-
-#Metals and Hardness
-metalsrpa<-c("Cyanide","Aluminum","Iron","Lead","Mercury","Nickel","Silver","Thallium","Antimony","Arsenic","Arsenic, Inorganic",
-             "Beryllium","Cadmium","Chromium","Copper","Zinc","Selenium","Nitrate","Inorganic nitrogen (nitrate and nitrite)",
-             "Nitrate + Nitrite","Chromium(III)","Chromium(VI)","Arsenic ion (3+)","Total hardness","Hardness, Ca, Mg",
-             "Hardness, carbonate","Hardness, non-carbonate","Ammonia ","Ammonia and ammonium","Ammonia-nitrogen")
-
-#all toxics (metals, voc, acid extractable, base neutral,pesticides and PCBs,metals)
-tox<-c(metalsrpa,vocrpa,aext,bneut,pestrpa)
+#only need date, org, and whether rejected or not for query
 
 
 # Check to see if saved cache of data exists. If it does not, or is greater than
@@ -98,7 +37,6 @@ if(!file.exists("query_cache.RData") |
 
 #NPDES_AWQMS_Stations functions only pulls stations 
 station <- NPDES_AWQMS_Stations()
-Mtype<-station$MonLocType
 station <- station$MLocID
 station <- sort(station)
 
@@ -106,7 +44,7 @@ organization <- AWQMS_Orgs()
 organization <- organization$OrganizationID
 organization <- sort(organization)
 
-save(station, Mtype, organization, file = 'query_cache.RData')
+save(station, organization, file = 'query_cache.RData')
 } else {
   load("query_cache.RData")
 }
