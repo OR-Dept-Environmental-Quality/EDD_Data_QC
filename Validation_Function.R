c1db67ee9aea59e1f239c1df404c257153fd7e8b
Aliana Britson <Britson.Aliana@deq.state.or.us>
2019-12-13 13:12:31 -0800

    fixed function so unecessary estimated data doesn't show up

--- Validation_Function.R
+++ Validation_Function.R
@@ -149,7 +149,7 @@ as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
   estm<-function(x){
     x<-namefrac(x)
     
-    sub<- subset(x,x$Result_Type=="Estimated" & x$Result_status!="Rejected" & (x$Result_Numeric>=x$MRLValue | x$Result_Numeric<=x$MDLValue),
+    sub<- subset(x,x$Result_Type=="Estimated" & x$Result_status!="Rejected" & (x$Result_Numeric>x$MRLValue),
                   select=c(MLocID,act_id,SampleStartDate,SampleStartTime,Char_Name,Char_Speciation,
                            CASNumber,Result,Result_Unit,Method_Code,Result_Comment,Result_Type,Result_status))
   }
